de6e673d530cd29e8c771d8c28aa2702b03218b2
daisy <8931428+daisyyou@user.noreply.gitee.com>
2025-05-18 15:26:56 +0800

    flex 设置根元素基准值

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import { createApp } from 'vue';
-import '@/styles/index.css';
-import App from '@/App.vue';
 import { createPinia } from 'pinia';
 import router from '@/router';
+
+import App from '@/App.vue';
+
+import '@/styles/index.css';
+
+import { useREm } from '@/utils/flexible.js';
 
 const app = createApp(App);
 
 app.use(createPinia()).use(router);
 
 app.mount('#app');
+
+useREm();
--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -10,16 +10,21 @@ export const isMobileTerminal = computed(() => {
   return width.value < PC_DEVICE_WINTH;
 });
 
-
 /**
  * 根据用户设备设置的rem基准值
  */
-export const useREm = ()=>{
+export const useREm = () => {
   // 最大的rem基准值
-  const MAX_FONT_SIZE = 40
+  const MAX_FONT_SIZE = 40;
 
   // 监听文档解析完成事件
-  document.addEventListener('DOMContentLoaded',()=>{
-    
-  })
-}
+  document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html');
+    // 屏幕宽度/10 为根元素的基准值
+    console.log(window.innerWidth);
+    let fontSize = window.innerWidth / 10;
+    fontSize = fontSize > MAX_FONT_SIZE ? MAX_FONT_SIZE : fontSize;
+    // 设置根元素的基准值
+    html.style.fontSize = fontSize + 'px';
+  });
+};
