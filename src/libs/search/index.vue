affa82587c837d52a18001a59ae2a49cccc3fa8a
daisy <8931428+daisyyou@user.noreply.gitee.com>
2025-05-21 22:23:47 +0800

    feat [libs] search逻辑补充

--- src/libs/search/index.vue
+++ src/libs/search/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class=" group relative p-0.5 rounded-xl border-white duration-500 hover:bg-red-100/40">
+    <div ref="containerTarget" class=" group relative p-0.5 rounded-xl border-white duration-500 hover:bg-red-100/40">
         <div>
             <!-- 图标 -->
             <daisy-svg-icon class=" w-1.5 h-1.5 absolute translate-y-[-50%] top-[50%] left-2" name="search"
@@ -7,21 +7,22 @@
             <!-- 输入框 -->
             <input
                 class=" block w-full h-[44px] pl-4 text-sm outline-0 bg-zinc-100 caret-zinc-400 rounded-xl text-zinc-900 tracking-wide font-semibold border border-zinc-100 duration-500 group-hover:bg-white group-hover:border-zinc-200 focus:border-red-300"
-                type="text" placeholder="搜索" />
+                type="text" placeholder="搜索" v-model="inputValue" @focus="onFocusHandler" @blur="onBlurHandler" />
             <!-- 清空icon -->
-            <daisy-svg-icon name="input-delete"
+            <daisy-svg-icon v-show="inputValue" @click="handleClear" name="input-delete"
                 class=" h-1.5 w-1.5 absolute translate-y-[-50%] top-[50%] right-9 duration-500 cursor-pointer"></daisy-svg-icon>
             <!-- 分割线 -->
             <div
                 class=" opacity-0 h-1.5 w-[1px] absolute translate-y-[-50%] top-[50%] right-[62px] duration-500 bg-zinc-200 group-hover:opacity-100">
             </div>
             <!-- 搜索btn -->
-            <daisy-button class=" absolute translate-y-[-50%] top-[50%] right-1" icon="search"
-                iconColor="#ffffff"></daisy-button>
+            <daisy-button @click="handleSearch"
+                class=" absolute translate-y-[-50%] top-[50%] right-1 rounded-xl duration-500 opacity-0 group-hover:opacity-100"
+                icon="search" iconColor="#ffffff"></daisy-button>
         </div>
         <!-- 下拉 -->
         <transition name="slide">
-            <div
+            <div v-if="$slots.dropdown" v-show="isFocus"
                 class=" max-h-[368px] w-full text-base overflow-auto bg-white absolute z-20 left-0 top-[56px] p-2 rounded border border-zinc-200 hover:shadow-3xl">
                 <slot name="dropdown"></slot>
             </div>
@@ -29,7 +30,60 @@
     </div>
 </template>
 <script setup >
+import { useVModel, onClickOutside } from '@vueuse/core'
 
+// 更新事件
+const EMIT_UPDATE_MODELVALUE = 'update:modelValue'
+// 触发搜索（点击或回车）事件
+const EMIT_SEARCH = 'search'
+// 删除所有文本事件
+const EMIT_CLEAR = 'clear'
+// 输入事件
+const EMIT_INPUT = 'input'
+// 获取焦点事件
+const EMIT_FOCUS = 'focus'
+// 失去焦点事件
+const EMIT_BLUR = 'blur'
+
+const props = defineProps({
+    modelValue: {
+        type: String,
+        required: true
+    }
+})
+const emits = defineEmits([
+    EMIT_UPDATE_MODELVALUE,
+    EMIT_CLEAR,
+    EMIT_INPUT,
+    EMIT_FOCUS,
+    EMIT_BLUR,
+    EMIT_SEARCH
+])
+
+const inputValue = useVModel(props)
+const isFocus = ref(false)
+
+const handleClear = () => {
+    emits(EMIT_UPDATE_MODELVALUE, '')
+}
+const handleSearch = () => {
+    isFocus.value = false
+    emits(EMIT_SEARCH)
+}
+
+const onFocusHandler = () => {
+    isFocus.value = true
+    emits(EMIT_FOCUS)
+}
+
+const onBlurHandler = () => {
+    emits(EMIT_BLUR)
+}
+
+const containerTarget = ref(null)
+onClickOutside(containerTarget, () => {
+    isFocus.value = false
+})
 </script>
 <style lang="scss" scoped>
 .slide-enter-active {
--- src/views/layout/components/header/header-search/index.vue
+++ src/views/layout/components/header/header-search/index.vue
@@ -1,9 +1,15 @@
 <template>
   <div class=" w-full">
-    <daisy-search></daisy-search>
+    <daisy-search v-model="inputValue">
+      <template #dropdown>
+        <div>
+          dropdown
+        </div>
+      </template>
+    </daisy-search>
   </div>
 </template>
 <script setup >
-
+const inputValue = ref("")
 </script>
 <style lang="scss" scoped></style>
