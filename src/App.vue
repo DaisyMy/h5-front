66a678ab1d8fc8a00d9deacabacb2ffe7cc3966d
daisy <8931428+daisyyou@user.noreply.gitee.com>
2025-05-23 23:06:03 +0800

    feat [mian] list列表

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-view></router-view>
+  <div class=" h-screen fixed top-0 left-0 w-screen">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script setup>
---
+++ src/views/main/components/list/item.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class=" bg-white dark:bg-zinc-900 xl:dark:bg-zinc-800 rounded pb-1 h-[280px] w-[230px]">
+        <div class="relative w-full rounded cursor-zoom-in group">
+            <img class=" w-full rounded bg-transparent" :src="data.photo">
+            <div
+                class="hidden opacity-0 w-full h-full bg-zinc-900/50 absolute top-0 left-0 rounded duration-300 group-hover:opacity-100 xl:block">
+                <daisy-button class=" absolute top-1.5 left-1.5">分享</daisy-button>
+                <daisy-button class=" absolute top-1.5 right-1.5" type="info" icon="heart"
+                    iconClass="fill-zinc-900 dark:fill-zinc-200" />
+                <daisy-button class=" absolute bottom-1.5 left-1.5 bg-zinc-100/70" type="info" size="small" icon="download"
+                    iconClass="fill-zinc-900 dark:fill-zinc-200" />
+                <daisy-button class=" absolute bottom-1.5 right-1.5 bg-zinc-100/70" type="info" size="small" icon="full"
+                    iconClass="fill-zinc-900 dark:fill-zinc-200" />
+            </div>
+        </div>
+        <p class="text-sm mt-1 font-bold text-zinc-900 dark:text-zinc-300 line-clamp-2 px-1">{{ data.title }}</p>
+        <div class=" flex items-center mt-1 px-1">
+            <img :src="data.avatar" class="w-2 h-2 rounded-full" />
+            <span class="text-sm text-zinc-500 ml-1">{{ data.author }}</span>
+        </div>
+    </div>
+</template>
+<script setup >
+const props = defineProps({
+    data: {
+        type: Object,
+        required: true
+    }
+})
+
+</script>
